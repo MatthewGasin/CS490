78c3bd6a2b37d06de4cb06c280f6f54c8964b6e2
Taranack <taranack@gmail.com>
2019-03-11 11:41:23 -0400

    Last day commit

--- BackEnd.php
+++ BackEnd.php
@@ -5,7 +5,7 @@ $request = 'default';
 if (isset($_POST['request_type'])) {
     $request = $_POST['request_type'];
 } else {
-    echo '<br>no POST found, was a request_type specified?<br>';
+    echo 'no POST found, was a request_type specified?';
     exit();
 }
 
@@ -32,11 +32,14 @@ switch ($request) {
     case 'new_exam':
         new_exam($json_obj, $conn);
         break;
-    case 'submit_exam':
+    case 'submit':
         submit_exam($json_obj, $conn);
         break;
     case 'query':
         query_question($json_obj, $conn);
+        break;
+    case 'get_scores':
+        get_score($json_obj, $conn);
         break;
     case 'release':
         release($json_obj, $conn);
@@ -68,8 +71,6 @@ $conn->close();
 //REQUEST METHODS
 function login($json_obj, $conn)
 {
-
-
     $username = $json_obj['username'];
     $password = $json_obj['password'];
 
@@ -88,13 +89,12 @@ function login($json_obj, $conn)
         $response = "instructor";
     }
 
-    echo $response;
+    echo trim($response);
 
 }
 
 function to_list($str)
 {
-
     $comma_index = strpos($str, ',');
     $arg1 = substr($str, 0, $comma_index);
     $arg2 = substr($str, $comma_index + 2);
@@ -108,7 +108,6 @@ function to_str($arr)
 
 function new_question($json_obj, $conn)
 {
-
     $json_obj = $json_obj["questions"];
     $key = array_keys($json_obj)[0];
     $json_obj = $json_obj[$key];
@@ -155,33 +154,51 @@ function new_question($json_obj, $conn)
 
 function new_exam($json_obj, $conn)
 {
+    $sql = "DELETE FROM EXAM_QUESTIONS;";
+    $conn->query($sql);
+    $sql = "DELETE FROM EXAM";
+    $conn->query($sql);
+
+    $result = "success";
     $sql = "INSERT INTO EXAM (EID, isReleased)
 			VALUES (\"Exam\", 0);";
-    $conn->query($sql);
-    foreach ($json_obj['questions'] as $question) {
-        $sql = "INSERT INTO EXAM_QUESTIONS (QID, EID)
-				VALUES (\"" . $question['func_name'] . "\", \"Exam\");";
-        $conn->query($sql);
+
+    if ($conn->query($sql) === false) {
+        $result = "fail";
     }
 
+    $points = $json_obj['points'];
+    $i = 0;
+    foreach ($json_obj['questions'] as $question) {
+        $sql = "INSERT INTO EXAM_QUESTIONS (QID, EID, Points)
+				VALUES (\"" . $question['func_name'] . "\", \"Exam\", " . $points[$i] . ");";
+        if($conn->query($sql) === false){
+            $result = "fail";
+        }
+        $i++;
+    }
+
+    echo $result;
 }
 
 function submit_exam($json_obj, $conn)
 {
     $key = array_keys($json_obj)[0];
-    $questions = array_keys($json_obj[$key]['questions']);
+    $questions = $json_obj[$key]['questions'];
     $points = $json_obj[$key]['points'];
     $answers = $json_obj[$key]['answers'];
     $comments = $json_obj[$key]['comments'];
+    $score = $json_obj[$key]["score"];
 
     $result = "success";
 
-    for($i = 0; $i < count($questions); $i++){
-       $sql = "INSERT INTO STUDENT_SCORES (Username, QID, Answer, Comments, Score)
-                VALUES ('" . $key . "', '" . $questions[$i] . "', '" . $answers[$i] . "', '" . $comments  . "', " . $points[$i]  . ");";
-       if($conn->query($sql) === false){
-           $result = "fail";
-       }
+    for ($i = 0; $i < count($questions); $i++) {
+        $sql = "INSERT INTO STUDENT_SCORES (Username, QID, Answer, Comments, Score)
+                VALUES ('" . $key . "', '" . $questions[$i + 1]["func_name"] . "', '" . $answers[$i] . "', '" . $comments . "', " . $score . "); ";
+        if ($conn->query($sql) === false) {
+            echo $conn->error;
+            $result = "fail";
+        }
     }
     echo $result;
 }
@@ -226,25 +243,30 @@ function query_question($json_obj, $conn)
             //Time for the hard stuff
             //inputs+outputs
             $puts = $conn->query(
-                "SELECT * 
+                "SELECT DISTINCT QID, Input, Output 
     		FROM PUTS
     		WHERE QID = \"" . $QID . "\"");
             $inputs = array();
             $outputs = array();
             while ($put = $puts->fetch_assoc()) {
-                array_push($inputs, to_list($put['Input']));
+                array_push($inputs, array(to_list($put['Input'])[0], to_list($put['Input'])[1]));
                 array_push($outputs, $put['Output']);
             }
 
+            $myObj["inputs"] = $inputs;
+            $myObj["expected_outputs"] = $outputs;
+
             //topics
             $topicsQuery = $conn->query(
-                "SELECT * 
+                "SELECT DISTINCT QID, Topic 
     		FROM TOPICS
     		WHERE QID = \"" . $QID . "\"");
             $topics = array();
             while ($topic = $topicsQuery->fetch_assoc()) {
                 array_push($topics, $topic['Topic']);
             }
+
+            $myObj["topics"] = $topics;
 
             $questions[$row["Name"]] = $myObj;
         }
@@ -257,13 +279,10 @@ function query_question($json_obj, $conn)
     }
 }
 
-function release($json_obj, $conn)
+function get_score($json_obj, $conn)
 {
-    $sql = "UPDATE EXAM
-			SET isReleased = 1";
-    $conn->query($sql);
     $sql = "SELECT * FROM STUDENT_SCORES
-            GROUP BY Username";
+            ORDER BY Username";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $finalObj = array();
@@ -272,80 +291,130 @@ function release($json_obj, $conn)
         $questions = array();
         $points = [];
         $answers = "";
-        $score = -1;
+        $score = 0;
         $comments = "";
 
         $first = true;
 
         while ($row = $result->fetch_assoc()) {
-            if($row["Username"] !== $key and $first === false){
-                $student = array("questions"=>$questions, "points"=>$points, "answers"=>$answers, "score"=>$score, "comments"=>$comments);
+            if ($first) {
+                $key = $row["Username"];
+                $first = false;
+            }
+
+            if ($row["Username"] !== $key) {
+                $student = array("questions" => $questions, "points" => $points, "answers" => $answers, "score" => $score, "comments" => $comments);
                 $finalObj[$key] = $student;
                 $key = $row["Username"];
-            }else{
-                if($first){
-                    $key = $row["Username"];
-                    $first = false;
-                }
-                array_push($points, $row["Score"]);
-                $answers .= $row["Answer"];
-                $score += $row["Score"];
-                $comments += $row["Comments"];
 
-                //Hunt down the question
-                $sql = "SELECT * FROM QUESTIONS WHERE Name = '" . $row["QID"] . "';";
-                $questionsQuery = $conn->query($sql);
-                if($questionsQuery->num_rows > 0){
-                    $question = array();
-                    $questionRow = $questionsQuery->fetch_assoc();
-                    $question["func_name"] = $questionRow["Name"];
-                    $question["arg_names"] = to_list($questionRow["Arguments"]);
-                    $question["description"] = $questionRow["Description"];
-                    $question["difficulty"] = $questionRow["Difficulty"];
+                $points = [];
+            }
 
-                    $puts = $conn->query(
-                        "SELECT * 
+            $answers .= $row["Answer"];
+            $score = $row["Score"];
+            $comments = $row["Comments"];
+            //echo "On user " . $row["Username"] . " with question " . $row["QID"] . "<br>";
+            //Hunt down the question
+            $sql = "SELECT * FROM QUESTIONS WHERE Name = '" . $row["QID"] . "';";
+            $questionsQuery = $conn->query($sql);
+            if ($questionsQuery->num_rows > 0) {
+                $question = array();
+                $questionRow = $questionsQuery->fetch_assoc();
+                //echo "Queried for question " . $questionRow["Name"] . "<br>";
+
+                $question["func_name"] = $questionRow["Name"];
+                $question["arg_names"] = to_list($questionRow["Arguments"]);
+                $question["description"] = $questionRow["Description"];
+                $question["difficulty"] = $questionRow["Difficulty"];
+
+                $puts = $conn->query(
+                    "SELECT DISTINCT QID, INPUT, OUTPUT
     		            FROM PUTS
     		            WHERE QID = \"" . $row["QID"] . "\"");
-                    $inputs = array();
-                    $outputs = array();
-                    while ($put = $puts->fetch_assoc()) {
-                        array_push($inputs, to_list($put['Input']));
-                        array_push($outputs, $put['Output']);
-                    }
+                $inputs = array();
+                $outputs = array();
+                while ($put = $puts->fetch_assoc()) {
+                    array_push($inputs, array(to_list($put['INPUT'])[0], to_list($put['INPUT'])[1]));
+                    array_push($outputs, $put['OUTPUT']);
+                }
 
-                    $question["inputs"] = $inputs;
-                    $question["expected_outputs"] = $outputs;
+                $question["inputs"] = $inputs;
+                $question["expected_outputs"] = $outputs;
 
-                    //topics
-                    $topicsQuery = $conn->query(
-                        "SELECT * 
+                //topics
+                $topicsQuery = $conn->query(
+                    "SELECT DISTINCT QID, Topic  
     		              FROM TOPICS
     		              WHERE QID = \"" . $row["QID"] . "\"");
-                    $topics = array();
-                    while ($topic = $topicsQuery->fetch_assoc()) {
-                        array_push($topics, $topic['Topic']);
-                    }
-
-                    $question["topics"] = $topics;
-                    $questions[$questionRow["Name"]] = $question;
+                $topics = array();
+                while ($topic = $topicsQuery->fetch_assoc()) {
+                    array_push($topics, $topic['Topic']);
                 }
+
+                $pointsQuery = $conn->query("
+                SELECT *
+                FROM EXAM_QUESTIONS 
+                WHERE QID = '" . $row["QID"] . "';"
+                );
+                while ($point = $pointsQuery->fetch_assoc()) {
+                    array_push($points, (int)$point["Points"]);
+                }
+
+                $question["topics"] = $topics;
+                $questions[$questionRow["Name"]] = $question;
             }
+
         }
 
-        $student = array("questions"=>$questions, "points"=>$points, "answers"=>$answers, "score"=>$score, "comments"=>$comments);
+        $student = array("questions" => $questions, "points" => $points, "answers" => $answers, "score" => $score, "comments" => $comments);
         $finalObj[$key] = $student;
 
         $myJSON = json_encode($finalObj);
         echo $myJSON;
-    }else{
+    } else {
         echo "No exams";
+    }
+}
+
+function release($json_obj, $conn)
+{
+    $flip = 0;
+    $sql = "SELECT * FROM EXAM";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ((int)$row['isReleased'] === 1) {
+                $flip = 0;
+                break;
+            } else {
+                $flip = 1;
+                break;
+            }
+        }
+    }
+
+    $sql = "UPDATE EXAM
+			SET isReleased = " . $flip;
+    if ($conn->query($sql) === false) {
+        echo trim('fail');
+    } else {
+        echo trim('success');
     }
 }
 
 function review_grade($json_obj, $conn)
 {
-    $username = $json_obj["Username"];
+    //Check if grades have been released
+    $isReleasedQuery = "SELECT * FROM EXAM";
+    $isReleasedResult = $conn->query($isReleasedQuery);
+    $isReleasedRows = $isReleasedResult->fetch_assoc();
+    if($isReleasedRows["isReleased"] === 0){
+        echo "not released";
+        return;
+    }
+
+
+    $username = trim($json_obj["username"]);
     $sql = "SELECT * FROM STUDENT_SCORES
             WHERE Username = '" . $username . "';";
     $result = $conn->query($sql);
@@ -356,72 +425,69 @@ function review_grade($json_obj, $conn)
         $questions = array();
         $points = [];
         $answers = "";
-        $score = -1;
+        $score = 0;
         $comments = "";
 
-        $first = true;
-
         while ($row = $result->fetch_assoc()) {
-            if ($row["Username"] !== $key and $first === false) {
-                $student = array("questions" => $questions, "points" => $points, "answers" => $answers, "score" => $score, "comments" => $comments);
-                $finalObj[$key] = $student;
-                $key = $row["Username"];
-            } else {
-                if ($first) {
-                    $key = $row["Username"];
-                    $first = false;
-                }
-                array_push($points, $row["Score"]);
-                $answers .= $row["Answer"];
-                $score += $row["Score"];
-                $comments += $row["Comments"];
+            $answers .= $row["Answer"];
+            $score = $row["Score"];
+            $comments = $row["Comments"];
 
-                //Hunt down the question
-                $sql = "SELECT * FROM QUESTIONS WHERE Name = '" . $row["QID"] . "';";
-                $questionsQuery = $conn->query($sql);
-                if ($questionsQuery->num_rows > 0) {
-                    $question = array();
-                    $questionRow = $questionsQuery->fetch_assoc();
-                    $question["func_name"] = $questionRow["Name"];
-                    $question["arg_names"] = to_list($questionRow["Arguments"]);
-                    $question["description"] = $questionRow["Description"];
-                    $question["difficulty"] = $questionRow["Difficulty"];
+            //Hunt down the question
+            $sql = "SELECT * FROM QUESTIONS WHERE Name = '" . $row["QID"] . "';";
+            $questionsQuery = $conn->query($sql);
+            if ($questionsQuery->num_rows > 0) {
+                $question = array();
+                $questionRow = $questionsQuery->fetch_assoc();
 
-                    $puts = $conn->query(
-                        "SELECT * 
+                $question["func_name"] = $questionRow["Name"];
+                $question["arg_names"] = to_list($questionRow["Arguments"]);
+                $question["description"] = $questionRow["Description"];
+                $question["difficulty"] = $questionRow["Difficulty"];
+
+                $puts = $conn->query(
+                    "SELECT DISTINCT QID, INPUT, OUTPUT
     		            FROM PUTS
     		            WHERE QID = \"" . $row["QID"] . "\"");
-                    $inputs = array();
-                    $outputs = array();
-                    while ($put = $puts->fetch_assoc()) {
-                        array_push($inputs, to_list(['Input']));
-                        array_push($outputs, $put['Output']);
-                    }
+                $inputs = array();
+                $outputs = array();
+                while ($put = $puts->fetch_assoc()) {
+                    array_push($inputs, array(to_list($put['INPUT'])[0], to_list($put['INPUT'])[1]));
+                    array_push($outputs, $put['OUTPUT']);
+                }
 
-                    $question["inputs"] = $inputs;
-                    $question["expected_outputs"] = $outputs;
+                $question["inputs"] = $inputs;
+                $question["expected_outputs"] = $outputs;
 
-                    //topics
-                    $topicsQuery = $conn->query(
-                        "SELECT * 
+                //topics
+                $topicsQuery = $conn->query(
+                    "SELECT DISTINCT QID, Topic  
     		              FROM TOPICS
     		              WHERE QID = \"" . $row["QID"] . "\"");
-                    $topics = array();
-                    while ($topic = $topicsQuery->fetch_assoc()) {
-                        array_push($topics, $topic['Topic']);
-                    }
-
-                    $question["topics"] = $topics;
-                    $questions[$questionRow["Name"]] = $question;
-
+                $topics = array();
+                while ($topic = $topicsQuery->fetch_assoc()) {
+                    array_push($topics, $topic['Topic']);
                 }
+
+                $pointsQuery = $conn->query("
+                SELECT *
+                FROM EXAM_QUESTIONS 
+                WHERE QID = '" . $row["QID"] . "';"
+                );
+                while ($point = $pointsQuery->fetch_assoc()) {
+                    array_push($points, (int)$point["Points"]);
+                }
+
+                $question["topics"] = $topics;
+                $questions[$questionRow["Name"]] = $question;
             }
         }
         $student = array("questions" => $questions, "points" => $points, "answers" => $answers, "score" => $score, "comments" => $comments);
-        $finalObj[$key] = $student;
-
+        $finalObj[$username] = $student;
         $myJSON = json_encode($finalObj);
         echo $myJSON;
+    }else{
+        echo $username . " has not taken an exam";
     }
 }
 
@@ -432,8 +498,9 @@ function take_exam($json_obj, $conn)
     if ($result->num_rows > 0) {
         $finalObj = array();
         $questions = array();
-        $points = [];
+        $points = array();
         while ($row = $result->fetch_assoc()) {
+            array_push($points, (int)$row['Points']);
             //Hunt down the question
             $sql = "SELECT * FROM QUESTIONS WHERE Name = '" . $row["QID"] . "';";
             $questionsQuery = $conn->query($sql);
@@ -446,13 +513,13 @@ function take_exam($json_obj, $conn)
                 $question["difficulty"] = $questionRow["Difficulty"];
 
                 $puts = $conn->query(
-                    "SELECT * 
+                    "SELECT DISTINCT QID, Input, Output 
     		            FROM PUTS
     		            WHERE QID = \"" . $row["QID"] . "\"");
                 $inputs = array();
                 $outputs = array();
                 while ($put = $puts->fetch_assoc()) {
-                    array_push($inputs, to_list(['Input']));
+                    array_push($inputs, array(to_list($put['Input'])[0], to_list($put['Input'])[1]));
                     array_push($outputs, $put['Output']);
                 }
 
@@ -461,7 +528,7 @@ function take_exam($json_obj, $conn)
 
                 //topics
                 $topicsQuery = $conn->query(
-                    "SELECT * 
+                    "SELECT DISTINCT QID, Topic 
     		              FROM TOPICS
     		              WHERE QID = \"" . $row["QID"] . "\"");
                 $topics = array();
@@ -471,7 +538,6 @@ function take_exam($json_obj, $conn)
 
                 $question["topics"] = $topics;
                 $questions[$questionRow["Name"]] = $question;
-
             }
         }
 
@@ -495,7 +561,7 @@ function is_released($json_obj, $conn)
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            if ($row['isReleased' === 1]) {
+            if ((int)$row['isReleased'] === 1) {
                 echo('true');
                 break;
             } else {
@@ -506,19 +572,21 @@ function is_released($json_obj, $conn)
     }
 }
 
-function modify_grade($json_obj, $conn){
+function modify_grade($json_obj, $conn)
+{
     $key = array_keys($json_obj)[0];
-    $score = $json_obj[$key]["Score"];
-    $sql = "UPDATE STUDENT_SCORES
-            SET Score = " . $score  .
-            "WHERE Username = '" . $key . "';";
+    $score = $json_obj[$key]["score"];
+    $comments = $json_obj[$key]["comments"];
 
+    $sql = "UPDATE STUDENT_SCORES
+            SET Score = " . $score . ", Comments = '" . $comments . "'" .
+            " WHERE Username = '" . $key . "';";
     if ($conn->query($sql) === TRUE) {
         echo "success";
     } else {
         echo "fail";
+        //echo $conn->error;
     }
-
 }
 
 ?>
